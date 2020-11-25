ae735fb7635a6d38adfba3a588249545e046bc0d
gamliel <gamliel.chemla@gmail.com>
2020-11-25 17:41:21 +0200

    bla

--- test.js
+++ test.js
@@ -8,3 +8,43 @@ http.createServer(function (req,res){
 
 
 console.log("tobewebn");
+var http = require('http')
+
+http.createServer(function (req,res){
+    res.write("hello")
+    res.end()
+}).listen(8000)
+
+
+
+console.log("tobewebn");
+var http = require('http')
+
+http.createServer(function (req,res){
+    res.write("hello")
+    res.end()
+}).listen(8000)
+
+
+
+console.log("tobewebn");
+var http = require('http')
+
+http.createServer(function (req,res){
+    res.write("hello")
+    res.end()
+}).listen(8000)
+
+
+
+console.log("tobewebn");
+var http = require('http')
+
+http.createServer(function (req,res){
+    res.write("hello")
+    res.end()
+}).listen(8000)
+
+
+
+console.log("tobewebn");
